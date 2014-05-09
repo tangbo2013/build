f9dc8647d022bafb9ac6035a45a6a61ea035c1d5
tangbo <tangbo@tiros.com.cn>
2014-05-09 16:37:55 +0800

    create init and install repo script.

---
+++ init-repo.sh
@@ -0,0 +1,6 @@
+mkdir xCPI
+cd xCPI
+
+repo init -u git@github.com:xCPI/manifest.git -b master --repo-url=git@github.com:xCPI/repo.git
+
+repo sync
---
+++ install-repo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+### 安装或更新 repo
+git clone git@github.com:xCPI/repo.git ## 克隆 repo 版本库
+cp repo/repo /usr/local/bin/repo ## 安装 repo 到本地系统中，我的 repo 安
+装目录是：/usr/local/bin/repo，某些系统执行这条命令时可能需要加上sudo
+
